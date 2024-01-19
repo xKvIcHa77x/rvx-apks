a6e2c7586f1567457932a7cf79b413847fbff5b6
krvstek <138402769+krvstek@users.noreply.github.com>
2024-01-19 12:42:14 +0100

    change MicroG app

--- build.sh
+++ build.sh
@@ -179,7 +179,7 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\n- 🔰 » Install [mMicroG](https://github.com/kazimmt/mMicroG/releases) for non-root YouTube and YT Music"
+log "\n- 🔰 » Install [MicroG-RE](https://github.com/WSTxda/MicroG-RE/releases) for non-root YouTube and YT Music"
 log "- 🔰 » Use [zygisk-detach (Magisk)](https://github.com/j-hc/zygisk-detach) module or [UpdateLocker (LSPosed)](https://github.com/Xposed-Modules-Repo/ru.mike.updatelocker/releases) to detach patched apps from being updated by Play Store"
 log "\n📜 » Dependencies:"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
